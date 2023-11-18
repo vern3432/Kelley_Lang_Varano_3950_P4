e418e95b94d81f1cc33f431848d49a397e54f5a0
Aidan_Varano <varanoab@merrimack.edu>
2023-11-18 16:21:54 -0500

    test database fixed

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const sql3 = require('better-sqlite3');
 console.log("imported")
 
-const   db = new sql3('memory2.db');
+const   db = new sql3('memory.db');
 const  csv = require('csv-parser');
 const   fs = require('fs');
 console.log("imported")
 // create table
 
 //  db.exec( 'DROP TABLE menuItems;' );
+db.exec('DROP TABLE IF EXISTS menuItemsuser');
 db.exec( 'CREATE TABLE IF NOT EXISTS menuItems (ISBN TEXT, Book_Title TEXT,Book_Author TEXT,Year_Of_Publication TEXT,Publisher TEXT,Image_URL_S TEXT,Image_URL_M TEXT, Image_URL_L TEXT );' );
 console.log("executed")
+
 const insrow = db.prepare('insert into menuItems (ISBN,Book_Title,Book_Author,Year_Of_Publication,Publisher,Image_URL_S,Image_URL_M,Image_URL_L) VALUES (?, ?,?,?,?,?,?,?)' );
-fs.createReadStream('books.csv')
+fs.createReadStream('data2.csv')
   .pipe(csv({"separator":","}))
   .on('data', (row) => {
     console.log(row)
