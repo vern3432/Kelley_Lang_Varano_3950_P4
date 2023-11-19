df429a2f27383f35717863f5955dbc7368df1760
Aidan_Varano <varanoab@merrimack.edu>
2023-11-18 22:48:51 -0500

    Very basic functionality for collection buttons

--- userscript.js
+++ userscript.js
@@ -18,7 +18,7 @@ console.log('db loaded')
 db.exec('DROP TABLE IF EXISTS users');
 //
 console.log('table dropped')
-db.exec( 'CREATE TABLE IF NOT EXISTS users (username TEXT,password TEXT,profile_url TEXT, bio TEXT);');
-const insrow = db.prepare('insert into users (username,password,profile_url,bio) VALUES (?, ?,?,?)');
-insrow.run('aidan','123','https://cdn3.iconfinder.com/data/icons/general-icons-set-1/100/13_profile-512.png','sup mane');
+db.exec( 'CREATE TABLE IF NOT EXISTS users (username TEXT,password TEXT,profile_url TEXT, bio TEXT,collection TEXT);');
+const insrow = db.prepare('insert into users (username,password,profile_url,bio,collectionsu) VALUES (?, ?,?,?,?)');
+insrow.run('aidan','123','https://cdn3.iconfinder.com/data/icons/general-icons-set-1/100/13_profile-512.png','sup mane','2005018');
 console.log("executed")
