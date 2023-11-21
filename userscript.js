83f6d3a34fd250cf20fff5dbd7b8d73be4adf835
Aidan_Varano <varanoab@merrimack.edu>
2023-11-20 23:05:01 -0500

    Backup

--- userscript.js
+++ userscript.js
@@ -20,7 +20,7 @@ db.exec('DROP TABLE IF EXISTS users');
 console.log('table dropped')
 db.exec( 'CREATE TABLE IF NOT EXISTS users (username TEXT,password TEXT,profile_url TEXT, bio TEXT,collection TEXT);');
 const insrow = db.prepare('insert into users (username,password,profile_url,bio,collection) VALUES (?, ?,?,?,?)');
-insrow.run('aidan','123','https://cdn3.iconfinder.com/data/icons/general-icons-set-1/100/13_profile-512.png','sup mane','2005018,887841740,080652121X,452264464');
+insrow.run('aidan','123','https://cdn3.iconfinder.com/data/icons/general-icons-set-1/100/13_profile-512.png','sup mane','2005018,887841740,080652121X,452264464,486284735');
 console.log("executed")
 
-"2005018","887841740","080652121X","452264464"
+// "2005018","887841740","080652121X","452264464"
